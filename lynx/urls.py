f6143e77bea273d79c06e090f5bcd5b9080c04c9
misua <charlespogzz@googlemail.com>
2023-12-24 20:44:08 +0800

    adding trial htmx button and bootstrap

--- lynx/urls.py
+++ lynx/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from.views import ArticleDeleteView
 from . import views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('delete/<int:pk>/', ArticleDeleteView.as_view(), name='article-delete'),
+    
 ]
--- lynx/views.py
+++ lynx/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render,HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
-from . models import Article
-# Create your views here.
+from .models import Article
 
 def home(request):
     articles = Article.objects.all()
     context = {
-        'articles':articles
+        'articles': articles
     }
-    return render  (request,'lynx/index.html',context=context)
+    return render(request, 'lynx/index.html', context=context)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ArticleDeleteView(View):
+    def delete(self, request, pk):
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return JsonResponse({'status':'success'}, status=200)
