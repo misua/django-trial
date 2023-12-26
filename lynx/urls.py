8a0b3bce4e827b158fe098b2326830f7aa36309f
misua <charlespogzz@googlemail.com>
2023-12-26 16:34:37 +0800

    whats happening?

--- lynx/urls.py
+++ lynx/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('delete/<int:pk>/', ArticleDeleteView.as_view(), name='article-delete'),
-    path('cancel_delete/', views.cancel_delete, name='cancel-delete'),
+    path('delete/<int:pk>/', ArticleDeleteView.as_view(), name='delete-view'),
+    # path('cancel_delete/', views.cancel_delete, name='cancel-delete'),
     path('article/<int:pk>/',views.singular_article, name='article'),
 ]
--- lynx/views.py
+++ lynx/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-# from django.utils.decorators import method_decorator
+from django.utils.decorators import method_decorator
 from django.shortcuts import render,redirect
 from django.views.generic import View
 
@@ -13,30 +13,35 @@ def home(request):
     }
     return render(request, 'lynx/index.html', context=context)
 
-# @method_decorator(csrf_exempt, name='dispatch')
-# class ArticleDeleteView(View):
-#     def delete(self, request, pk):
-#         article = Article.objects.get(pk=pk)
-#         article.delete()
-#         # return JsonResponse({'status':'success'}, status=200)
-#         return render(request, 'lynx/delete.html')
-
-def cancel_delete(request):
-    return HttpResponse()
-
+@method_decorator(csrf_exempt, name='dispatch')
 class ArticleDeleteView(View):
     def get(self, request, pk):
         try:
             article = Article.objects.get(pk=pk)
             return render(request, 'lynx/delete.html',{'article':article})
-            return JsonResponse({'status':'success'}, status=200)
         except Article.DoesNotExist:
             return JsonResponse({'status':'error'}, status=400)
-        
+    
     def post(self, request, pk):
         article = Article.objects.get(pk=pk)
         article.delete()
         return redirect('home')
+
+# def cancel_delete(request):
+#     return HttpResponse()
+
+# class ArticleDeleteView(View):
+#     def get(self, request, pk):
+#         try:
+#             article = Article.objects.get(pk=pk)
+#             return render(request, 'lynx/delete.html',{'article':article})
+#         except Article.DoesNotExist:
+#             return JsonResponse({'status':'error'}, status=400)
+        
+#     def post(self, request, pk):
+#         article = Article.objects.get(pk=pk)
+#         article.delete()
+#         return redirect('home')
     
 def singular_article(request, pk):
     article = Article.objects.get(id=pk)
